f3243f0aea2c90aa7fbd84277a185bc8f372a550
jcastelain <castelain.jeremy89@gmail.com>
2018-04-27 23:18:34 +0200

    finished

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-require('dotenv').config();
+require('dotenv').config(); // récupère les variables d'environnement
 
-var builder = require('botbuilder');
-var restify = require('restify');
+var builder = require('botbuilder'); // composant du SDK microsoft bot builder
+var restify = require('restify'); // web service basé sur Node.js
 
 // server
-var server = restify.createServer();
-server.listen(process.env.PORT, function() {
-    console.log("%s listening to %s", server.name, server.url);
+var server = restify.createServer(); // création du serveur web
+server.listen(process.env.PORT, function() { // attribution du port 3978
+    console.log("%s listening to %s", server.name, server.url); 
 });
 
 // Connector
@@ -15,41 +15,47 @@ var connector = new builder.ChatConnector({
     appPassword : process.env.MICROSOFT_APP_PASSWORD
 });
 
-server.post('/api/messages', connector.listen());
+server.post('/api/messages', connector.listen()); // connecteur relié à l'url http://localhost:3978/api/messages
 
-var inMemoryStorage = new builder.MemoryBotStorage();
+var inMemoryStorage = new builder.MemoryBotStorage(); // instanciation d'une mémoire à long terme
 
 // Universal bot
 var bot = new builder.UniversalBot(connector, [
     function(session){
-        session.send("Hey");
-        
-        //session.beginDialog('greetings', session.userData.profile);
-        session.beginDialog('menu');
+        var name = "buddy";
+        if(session.userData.profile){
+            name = session.userData.profile.name; // si l'utilisateur n'a pas de nom lui dire buddy
+        }
+        session.send(`Hey ${name}, it's decision time`); // 1ère réponse du bot une fois poké et récupération du nom si il a été saisi
+        session.beginDialog('menu'); // appel de la fonction de dialogue nommée menu 
     },
     function(session, results){
         if(!session.userData.profile){
-            session.userData.profile = results.response;
+            session.userData.profile = results.response; // on récupère l'information du endDialogueWithResult de la step 2
         }
-        session.send(`hello ${session.userData.profile.name} :-)`);
+        session.send(`hello ${session.userData.profile.name} =)`); // on affiche le nom saisi par l'utilisateur
     }
-]).set('storage', inMemoryStorage);
+]).set('storage', inMemoryStorage); // attribution de la mémoire à long terme au bot
+
+// tableau clé valeure
+// Ici les valeurs du panneau de choix faisant référence à d'autres dialogues
 var menuItems ={
-    "toto": {
-        item: 'dialog1'
+    "Ask me a question": {
+        item: 'greetings'
     },
-    "tata": {
-        item: 'dialog2'
+    "Blague Toto": {
+        item: 'blague'
     },
-    "titi": {
-        item: 'dialog3'
+    "Quitter": {
+        item: 'quitter'
     }
 }
+
+// affichage du panneau de choix + clique vers le choix du dialogue sélectionné
 bot.dialog('menu', [
     function(session){
-        builder.Prompts.choice(session, 'select an option', menuItems, {listStyle: 3});
+        builder.Prompts.choice(session, 'Select an option', menuItems, {listStyle: 3}); // 2ème réponse du bot
     },
-    
     function(session, results){
         var choice = results.response.entity;
         var item = menuItems[choice].item;
@@ -57,39 +63,41 @@ bot.dialog('menu', [
     }
 ]);
 
-bot.dialog('getCompanyInfo', [
-    function(session){
-        session.send('You are inside dialog1');
-    }
-]);
-
-bot.dialog('dialog2', [
-    function(session){
-        session.send('You are inside dialog2');
-    }
-]);
-
-bot.dialog('dialog3', [
-    function(session){
-        session.send('You are inside dialog3');
-    }
-]);
-
+// Si clique sur Ask me a question exécute le dialogue "greetings"
 bot.dialog('greetings', [
     // Step 1
     function(session, results, skip){
         session.dialogData.profile = results || {};
-        if(!session.dialogData.profile.name){
-            builder.Prompts.text(session, 'what\'s your name ?');
-        }else{
+        if(!session.dialogData.profile.name){ // si le nom n'a pas été enregistrer en mémoire alors le bot le demande
+            builder.Prompts.text(session, 'What\'s your name ?');
+        }else{ // sinon on passe à la step 2
             skip();
         }
     },
     // Step 2
     function(session, results){
         if(results.response){
-            session.dialogData.profile.name = results.response;
+            session.dialogData.profile.name = results.response; // on récupère le nom que l'utilisateur saisi
         }
-        session.endDialogWithResult({ response: session.dialogData.profile });
+        session.endDialogWithResult({ response: session.dialogData.profile }); // on passe l'information de la réponse en quittant le dialogue
+    }
+]);
+
+// Si clique sur Blague Toto exécute le dialogue "blague"
+bot.dialog('blague', [
+    function(session, results, skip){
+        session.send('- Bonjour Toto<br>- Bonjour mémé.<br>- Si tu me dis combien j\'ai de bonbons dans ma main, je te les donne tous les deux.<br>- Tu en as deux ! - Qui te l\'a dit ?!? ');
+        session.endDialog();
+    }
+]);
+
+// Si clique sur Quitter clôture la discussion et affiche un message
+bot.dialog('quitter', [
+    function(session){
+        var nameAgain = ";)";
+        if(session.userData.profile){
+            nameAgain = session.userData.profile.name;
+        }
+        session.endConversation(`See you soon ${nameAgain} !`);
     }
 ]);
